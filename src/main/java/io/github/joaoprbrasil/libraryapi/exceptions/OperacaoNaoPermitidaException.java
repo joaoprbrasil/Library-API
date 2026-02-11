72433aca8410132000749b7a40e4ebd9270e349a
joaoprbrasil <joaopedrobrasil.brasil007@gmail.com>
2026-02-11 16:42:25 -0300

    add: controller routes

---
+++ src/main/java/io/github/joaoprbrasil/libraryapi/exceptions/OperacaoNaoPermitidaException.java
@@ -0,0 +1,7 @@
+package io.github.joaoprbrasil.libraryapi.exceptions;
+
+public class OperacaoNaoPermitidaException extends RuntimeException {
+    public OperacaoNaoPermitidaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/joaoprbrasil/libraryapi/service/AutorService.java
@@ -0,0 +1,72 @@
+package io.github.joaoprbrasil.libraryapi.service;
+
+import io.github.joaoprbrasil.libraryapi.exceptions.OperacaoNaoPermitidaException;
+import io.github.joaoprbrasil.libraryapi.model.Autor;
+import io.github.joaoprbrasil.libraryapi.repository.AutorRepository;
+import io.github.joaoprbrasil.libraryapi.repository.LivroRepository;
+import io.github.joaoprbrasil.libraryapi.validator.AutorValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AutorService {
+
+    private final AutorRepository repository;
+    private final AutorValidator validator;
+    private final LivroRepository livroRepository;
+
+
+    public AutorService(AutorRepository repository,
+                        AutorValidator validator,
+                        LivroRepository livroRepository){
+        this.repository = repository;
+        this.validator = validator;
+        this.livroRepository = livroRepository;
+    }
+
+    public Autor salvar(Autor autor){
+        validator.validar(autor);
+        return repository.save(autor);
+    }
+
+    public void atualizar(Autor autor){
+        if(autor.getId() == null){
+            throw new IllegalArgumentException("Para atualizar é necessário que o autor já esteja salvo no banco de dados");
+        }
+        repository.save(autor);
+    }
+
+    public Optional<Autor> buscaPorId(UUID id){
+        return repository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        if(possuiLivro(autor)){
+            throw new OperacaoNaoPermitidaException(
+                    "Não é permitido excluir um Autor que possui livros cadastrados."
+            );
+        }
+        repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+        if(nome != null){
+            return repository.findByNome(nome);
+        }
+        if(nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+        return repository.findAll();
+    }
+
+    public boolean possuiLivro(Autor autor){
+        return livroRepository.existsByAutor(autor);
+    }
+}
